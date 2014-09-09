78888dbeb8c2d27cb5dfb88bda5cb23e22a22d53
Veiko Sinivee <veiko.sinivee@gmail.com>
2014-09-09 11:25:30 +0300

    #23861 jdigidoc packaging with git

--- packaging/build-jdigidoc.rb
+++
@@ -1,9 +0,0 @@
-def replace_in_file filename, regex, value
-  spec = File.read(filename)
-  File.open(filename, "w") do |file|
-    file.write spec.gsub(regex, value)
-  end
-end
-
-puts "================= #{ARGV[0]}"
-replace_in_file "./jdigidoc/jdigidoc/src/main/java/ee/sk/digidoc/Version.java", /LIB_VERSION.*/, "LIB_VERSION = \"#{ARGV[0]}\";"
